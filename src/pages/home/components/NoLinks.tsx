ff042c4cdcdaab66d9b9d7ca122575b29d00812c
KaiserX <wiz0isaac@gmail.com>
2024-07-26 17:10:42 +0100

    nolinks fix

--- src/pages/home/components/NoLinks.tsx
+++ src/pages/home/components/NoLinks.tsx
@@ -3,14 +3,14 @@ import NoLinksIcon from "@/assets/platformicons/NoLinksIcon";
 
 export default function NoLinks() {
   return (
-    <div className="grid justify-items-center gap-6 m-5 mt-11">
-      <div className="w-32 h-auto">
+    <div className="flex flex-col items-center justify-center text-center gap-6 m-5 mt-11">
+      <div className="h-auto">
         <NoLinksIcon />
       </div>
 
       <h3 className="text-xl font-bold">Let's get you started</h3>
 
-      <p className="text-center max-w-[255px] text-sm text-gray-500">
+      <p className="max-w-[255px] text-sm text-gray-500">
         Use the "Add new link" button to get started. Once you have more than
         one link, you can reorder and edit them. We're here to help you share
         your profiles with everyone!
